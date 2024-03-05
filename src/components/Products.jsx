ac29a8afdc9e587d18e60bedc41305ea9cfd591e
Harsh sheladiya <shv245245@gmail.com>
2024-03-05 12:45:28 +0530

    e-mart

---
+++ src/components/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import  Product  from './Product'
+import { popularProducts } from '../data'
+
+const Conatiner = styled.div`
+    padding: 20px;
+    display: flex;
+
+`
+
+
+
+
+ const Products = () => {
+  return (
+    <Conatiner>
+        {popularProducts.map((item) => (
+            <Product item={item} key={item.id}/>
+        ))}
+    </Conatiner>
+)}
+
+export default Products;
