80dbd6562becef9525f5b4bfda30e2ec7855fcf6
Ritesh S Goriya <200120116018@git.org.in>
2024-04-01 16:45:53 +0530

    e-mart

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const sliderItems = [
     },
     {
       id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
+      img: "https://i.ibb.co/vVPkwSG/ab95ab2e56830b8534b71fefad0b0f10-1-removebg-preview.png",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
@@ -25,17 +25,17 @@ export const sliderItems = [
   export const categories = [
     {
       id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "https://assets.myntassets.com/fl_progressive/h_960,q_80,w_720/v1/assets/images/2164373/2017/11/15/11510749192975-Roadster-Men-Brown--Black-Regular-Fit-Checked-Casual-Shirt-8071510749192783-1.jpg",
       title: "SHIRT STYLE!",
     },
     {
       id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "https://cdn.luxe.digital/media/2020/03/18150853/men-loungewear-style-modern-luxury-luxe-digital.jpg",
       title: "LOUNGEWEAR LOVE",
     },
     {
       id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.opumo.com/wordpress/wp-content/uploads/2023/05/Wax15-970x1200.jpg",
       title: "LIGHT JACKETS",
     },
   ];
@@ -43,19 +43,19 @@ export const sliderItems = [
   export const popularProducts = [
     {
       id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img:"https://i5.walmartimages.com/seo/Short-Sleeve-Shirt-Traditional-Pattern-Print-Super-Cool-Breathable-Vivid-Design-Summer-Shirt-for-Men-Women-for-Gift-to-Friens_627c52fc-2dff-40a9-af5a-5fd0a1f9ce15.b39e85e09b037bdefbe26789768221c1.png?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
     },
     {
       id:2,
-      img:"https://i.ibb.co/HFvbVmq/istockphoto-1144580520-612x612-removebg-preview.png",
+      img:"https://pbx2-pbww-prod-pbww-cdn.getprintbox.com/media/productimage/e925de22-44e0-4276-bb92-0b4101f5f834/Blank%20Jacket_thumb_900x900?mt=1583407059.733",
     },
     {
       id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      img:"https://in.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-sequin-bomber-jacket--FQVB02DFS630_PM2_Front%20view.jpg",
     },
     {
       id:4,
-      img:"https://i.ibb.co/SxG99Lk/e9107cd5-694e-432f-8a14-d4a8232f33661679909995981-DKGF-FASHION-Boys-Single-Breasted-5-Piece-Suit-762.png",
+      img:"https://d1fufvy4xao6k9.cloudfront.net/feed/img/man_smoking/873649/without_model_small.png",
     },
     {
       id:5,
@@ -73,8 +73,4 @@ export const sliderItems = [
       id:8,
       img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
     },
-    // {
-    //   id:9,
-    //   img:""
-    // }
   ]
