76c1ac748d3a011fd0154be3fa15c81a36a703ae
Ritesh S Goriya <200120116018@git.org.in>
2024-04-16 11:43:04 +0530

    Slider aded in mobile

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,56 +1,76 @@
-import React from 'react'
-import  styled  from 'styled-components'
-import {mobile} from '../responsive'
+import React from 'react';
+import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
-width: 100%;
-margin: 3px;
-height: 70vh;
-position: relative;
+  width: 100%;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+
+  ${mobile({
+    height: "auto",
+    marginBottom: "20px" // Adjusted margin for mobile
+  })}
 `;
+
 const Image = styled.img`
- width: 100%;
- height: 100%;
- object-fit: cover;
- ${mobile({ height: "100%" })}
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  ${mobile({
+    height: "100%"
+  })}
 `;
+
 const Info = styled.div`
-position : absolute;
-top:0;
-left: 0;
-width: 100%;
-height: 100%;
-display:flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-
-`
-const Title = styled.div`
-  color:white;
-  margin-bottom: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
-const Button = styled.div`
- border:none;
-    padding: 10px;
-    background-color: white;
-    color:gray;
-    cursor: pointer;
-    font-weight: 600;
-`
 
- 
-const CategoryItem = ({item}) => {
+const Title = styled.div`
+  color: white;
+  margin-bottom: 20px;
+  font-size: 24px; // Adjusted font size for mobile
+
+  ${mobile({
+    fontSize: "20px"
+  })}
+`;
+
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 16px; // Adjusted font size for mobile
+
+  ${mobile({
+    fontSize: "14px",
+    padding: "8px 16px" // Adjusted padding for mobile
+  })}
+`;
+
+const CategoryItem = ({ item }) => {
   return (
-   <Container>
-
-    <Image src={item.img}/> 
-      <Info> 
+    <Container>
+      <Image src={item.img} />
+      <Info>
         <Title>{item.title}</Title>
-        <Button> Shop Now </Button>
-    </Info>
-   </Container>
-  )
-}
+        <Button>Shop Now</Button>
+      </Info>
+    </Container>
+  );
+};
 
-export default CategoryItem
+export default CategoryItem;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,5 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { sliderItems } from "../data";
 import { mobile } from "../responsive";
@@ -10,7 +10,7 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  ${mobile({ display: "none"})}
+  ${mobile({ height: "70vh", overflow: "hidden" })}
 `;
 
 const Arrow = styled.div`
@@ -24,12 +24,11 @@ const Arrow = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: ${(props) => props.direction === "left" && "10px"};
-  right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
   z-index: 2;
+  ${mobile({ display: "none" })}
 `;
 
 const Wrapper = styled.div`
@@ -40,53 +39,68 @@ const Wrapper = styled.div`
 `;
 
 const Slide = styled.div`
-  width: 100vw;
+  min-width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
   background-color: #${(props) => props.bg};
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ImgContainer = styled.div`
   height: 100%;
   flex: 1;
+  ${mobile({ height: "90%", width: "90%" })}
 `;
 
 const Image = styled.img`
   height: 80%;
+  ${mobile({ height: "100%", width: "100%", objectFit: "cover" })}
 `;
 
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
+  ${mobile({ padding: "20px" })}
 `;
 
 const Title = styled.h1`
-  font-size: 70px;
+  font-size: 30px;
+  ${mobile({ fontSize: "20px" })}
 `;
 
 const Desc = styled.p`
-  margin: 50px 0px;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 3px;
+  margin: 20px 0px;
+  font-size: 16px;
+  ${mobile({ fontSize: "14px" })}
 `;
 
 const Button = styled.button`
   padding: 10px;
-  font-size: 20px;
+  font-size: 16px;
   background-color: transparent;
   cursor: pointer;
+  ${mobile({ fontSize: "14px" })}
 `;
+
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
+
   const handleClick = (direction) => {
     if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+      setSlideIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : sliderItems.length - 1));
     } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+      setSlideIndex((prevIndex) => (prevIndex < sliderItems.length - 1 ? prevIndex + 1 : 0));
     }
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleClick("right");
+    }, 5000); // Change slide every 5 seconds (5000 milliseconds)
+
+    return () => clearInterval(interval);
+  }, []); // Run effect only once on mount
 
   return (
     <Container>
